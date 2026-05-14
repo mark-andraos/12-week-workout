49819dccc75d31f6012dc047189d16ee09d7ac73
Mark <andraos1992@gmail.com>
2026-05-15 07:21:09 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = '12ww-v1';
+const ASSETS = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('script.google.com')) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
